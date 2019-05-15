ed3ebc3b205d007be4545247a0a39e55100b9fe9
Michael Neufeld <mneufeld@akamai.com>
2019-05-15 17:29:07 -0400

    WORM RW node overhaul now maintains internal buffer of header

--- include/RadixTree/BinaryWORMNodeHeader.h
+++ include/RadixTree/BinaryWORMNodeHeader.h
@@ -28,9 +28,6 @@ SOFTWARE.
 #include <stdexcept>
 #include <limits>
 
-#include "RadixTreeUtils.h"
-#include "SimpleEdge.h"
-
 #include "BinaryWORMNodeHeaderBytes.h"
 
 namespace Akamai {
@@ -97,70 +94,82 @@ private:
  */
 template <std::size_t OFFSETSIZE,bool LITTLEENDIAN>
 class BinaryWORMNodeHeaderRW
-  : public BinaryWORMNodeHeaderRO<OFFSETSIZE,LITTLEENDIAN>
 {
 public:
-  using MyType = BinaryWORMNodeHeaderRW<OFFSETSIZE,LITTLEENDIAN>;
-  using HeaderRO = BinaryWORMNodeHeaderRO<OFFSETSIZE,LITTLEENDIAN>;
   using HeaderBytes = BinaryWORMNodeHeaderBytes<OFFSETSIZE,LITTLEENDIAN>;
+  using MyType = BinaryWORMNodeHeaderRW<OFFSETSIZE,LITTLEENDIAN>;
   using OffsetType = typename HeaderBytes::OffsetType;
-  // XXX replace with wrapper for byte edge...
-  // XXX look out for copy assignment - that kind of thing
-  class EdgeType {
-  public:
-    EdgeType(uint8_t* edgeByte) : edgeByte_(edgeByte) {}
-    EdgeType& operator=(const EdgeType& o) {
-      *edgeByte_ = (*edgeByte_ & HeaderBytes::MASK_ALL_EDGE_OUT) | (*(o.edgeByte_) & HeaderBytes::MASK_ALL_EDGE_IN);
-    }
-
-    bool full() const { return HeaderBytes::edgeStepCount(edgeByte_.data()) == HeaderBytes::MaxEdgeSteps; }
-    void pushBack(std::size_t step) {
-      if (full()) {
-        // XXX throw
-      }
-      std::size_t curStepCount = HeaderBytes::edgeStepCount(edgeByte_.data());
-      HeaderBytes::setEdgeStepAt(curStepCount,step);
-      HeaderBytes::setEdgeStepCount(edgeByte_.data(),++curStepCount);
-    }
-    
-
-  private:
-    uint8_t* edgeByte_{nullptr};
-  };
   static constexpr std::size_t Radix = 2;
   static constexpr std::size_t OffsetSize = OFFSETSIZE;
+  static constexpr bool LittleEndian = LITTLEENDIAN;
+  static constexpr bool BigEndian = !LITTLEENDIAN;
+  static constexpr std::size_t MaxEdgeSteps = HeaderBytes::MaxEdgeSteps;
+  using EdgeWordType = typename HeaderBytes::EdgeWordType;
+  static constexpr std::size_t NoChild = std::numeric_limits<std::size_t>::max();
 
-  BinaryWORMNodeHeaderRW() : HeaderRO(headerBytes_.data()) {}
+  static std::string headerTypeID() { return HeaderBytes::headerTypeID(); }
+
+  bool hasChild(std::size_t c) const {
+    if (c >= Radix) { throw std::runtime_error("BinaryWORMNodeHeaderRW: child out of range"); }
+    return HeaderBytes::hasChild(headerBytes_.data(),c);
+  }
+  bool hasValue() const { return HeaderBytes::hasValue(headerBytes_.data()); }
+
+  std::size_t edgeStepCount() const { return HeaderBytes::edgeStepCount(headerBytes_.data()); }
+  EdgeWordType edgeBitsAsWord() const { return HeaderBytes::getEdgeBitsAsWord(headerBytes_.data()); }
+  std::size_t edgeStepAt(std::size_t es) const {
+    if (es >= HeaderBytes::edgeStepCount(headerBytes_.data())) { throw std::runtime_error("BinaryWORMNodeHeaderRW: edge step out of range"); }
+    return HeaderBytes::edgeStepAt(headerBytes_.data(),es);
+  }
+  std::size_t headerSize() const { return HeaderBytes::headerSize(headerBytes_.data()); }
+  // Without knowing the size of the value we can't
+  // actually compute the left node offset since it
+  // directly follows our node header + value.
+  OffsetType rightChildOffset() const {
+    if (!(hasChild(0) && hasChild(1))) { throw std::runtime_error("BinaryWORMNodeHeaderRW: right child offset doesn't exist"); }
+    return HeaderBytes::getRightChildOffset(headerBytes_.data());
+  }
 
   void setHasValue(bool hv) { HeaderBytes::setHasValue(headerBytes_.data(),hv); }
 
   void setHasChild(std::size_t c,bool hc) {
-    // XXX check limits and throw
+    if (c > 1) { throw std::runtime_error("BinaryWORMNodeHeaderRW::setHasChild: child out of range"); }
     HeaderBytes::setHasChild(headerBytes_.data(),c,hc);
   }
+  void setHasChild(const std::array<bool,2>& hc) { 
+    HeaderBytes::setHasChild(headerBytes_.data(),0,hc[0]);
+    HeaderBytes::setHasChild(headerBytes_.data(),1,hc[1]);
+  }
   void setRightChildOffset(OffsetType rco) {
-    if (!(hasChild(0) && hasChild(1))) { throw std::runtime_error("BinaryWORMNodeHeaderRW: cannot set right child offset without right child present"); }
+    if (!(this->hasChild(0) && this->hasChild(1))) { throw std::runtime_error("BinaryWORMNodeHeaderRW: cannot set right child offset without right child present"); }
     HeaderBytes::setRightChildOffset(headerBytes_.data(),rco);
   }
 
-  EdgeType& edge() { return edge_; }
-  const EdgeType& edge() const { return edge_; }
-
+  bool edgeFull() const { return HeaderBytes::edgeStepCount(headerBytes_.data()) == HeaderBytes::MaxEdgeSteps; }
+  void edgePushBack(std::size_t step) {
+    if (edgeFull()) {
+      throw std::runtime_error("BinaryWORMNodeHeaderRW::edgePushBack: edge full");
+    }
+    std::size_t curStepCount = HeaderBytes::edgeStepCount(headerBytes_.data());
+    HeaderBytes::setEdgeStepAt(headerBytes_.data(),curStepCount,step);
+    HeaderBytes::setEdgeStepCount(headerBytes_.data(),++curStepCount);
+  }
+  void copyEdgeFrom(const MyType& o) {
+    headerBytes_[0] = (headerBytes_[0] & HeaderBytes::MASK_ALL_EDGE_OUT) | (o.headerBytes_[0] & HeaderBytes::MASK_ALL_EDGE_IN);
+  }
   std::size_t writeHeader(uint8_t* b) const {
     if (b == nullptr) { throw std::runtime_error("BinaryWORMNodeHeaderRW: attempt to write to nullptr"); }
-    std::memcpy(b,headerBytes_.data(),HeaderBytes::headerSize(b));
+    std::memcpy(b,headerBytes_.data(),HeaderBytes::headerSize(headerBytes_.data()));
     return HeaderBytes::headerSize(b);
   }
   std::size_t readHeader(const uint8_t* b) {
-    // XXX memcpy into our own buffer
     if (b == nullptr) { throw std::runtime_error("BinaryWORMNodeHeaderRW: attempt to read from nullptr"); }
     std::memcpy(headerBytes_.data(),b,HeaderBytes::headerSize(b));
     return HeaderBytes::headerSize(b);
   }
 
 private:
-  std::array<uint8_t,HeaderBytes<OFFSETSIZE,LITTLEENDIAN>::MaxHeaderSize> headerBytes_{};
-  EdgeType edge_{headerBytes_.data()};
+  std::array<uint8_t,HeaderBytes::MaxHeaderSize> headerBytes_{};
 };
 
 /////////////////////
